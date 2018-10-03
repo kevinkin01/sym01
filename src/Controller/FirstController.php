18e02a1b4b872bfbb0ba146ccb813dff2b0e2f79
kevinkin01 <kevinkinanga41@gmail.com>
2018-10-03 12:15:48 +0200

    third page

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
 class FirstController extends AbstractController
 {
     /**
@@ -15,5 +12,19 @@ class FirstController extends AbstractController
         return $this->render('first/index.html.twig', [
             'controller_name' => 'FirstController',
         ]);
+    }
+    /**
+     * @Route("/", name="Accueil")
+     */
+    public function accueilAction(){
+        // render the view
+        return $this->render('first/accueil.html.twig');
+    }
+    /**
+     * @Route("/lulu", name="third")
+     */
+    public function thirdAction(){
+        // render the view
+        return $this->render('first/third.html.twig');
     }
 }
